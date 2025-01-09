33b1475e62c8d54496d72e604369896eeba1d919
Vanessazanco <vanessazancof@gmail.com>
2025-01-09 11:54:59 +0000

    first commit

---
+++ src/main/java/clinica/med/apiClinica/ApiClinicaApplication.java
@@ -0,0 +1,13 @@
+package clinica.med.apiClinica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiClinicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiClinicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/clinica/med/apiClinica/ApiClinicaApplicationTests.java
@@ -0,0 +1,13 @@
+package clinica.med.apiClinica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiClinicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
