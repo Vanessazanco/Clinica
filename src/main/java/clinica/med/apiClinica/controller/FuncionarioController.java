f96ecfb0138a7304fb328f0f59c97a0aceaa9d20
Vanessazanco <vanessazancof@gmail.com>
2025-01-10 13:05:33 +0000

    Controller de Funcionario

--- src/main/java/clinica/med/apiClinica/controller/FuncionarioController.java
+++ src/main/java/clinica/med/apiClinica/controller/FuncionarioController.java
@@ -1,10 +1,15 @@
 package clinica.med.apiClinica.controller;
 
 import clinica.med.apiClinica.funcionario.DadosCadastroFuncionario;
+import clinica.med.apiClinica.funcionario.DadosListarFuncionario;
 import clinica.med.apiClinica.funcionario.Funcionario;
 import clinica.med.apiClinica.funcionario.FuncionarioRepository;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("funcionarios")
@@ -14,8 +19,13 @@ public class FuncionarioController {
     private FuncionarioRepository repository;
 
     @PostMapping
-    public void cadastrar(@RequestBody DadosCadastroFuncionario dados){
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroFuncionario dados){
         repository.save(new Funcionario(dados));
+    }
+    @GetMapping
+    public List<DadosListarFuncionario> listar(){
+        return repository.findAll();
     }
 
 }
