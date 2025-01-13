40eb825612878c4b5b40ceda8dc98eef547bc312
Vanessazanco <vanessazancof@gmail.com>
2025-01-13 11:15:12 +0000

    Cadastro de Pacientes

--- src/main/java/clinica/med/apiClinica/controller/PacienteController.java
+++ src/main/java/clinica/med/apiClinica/controller/PacienteController.java
@@ -2,6 +2,11 @@ package clinica.med.apiClinica.controller;
 
 
 import clinica.med.apiClinica.paciente.DadosCadastroPaciente;
+import clinica.med.apiClinica.paciente.Paciente;
+import clinica.med.apiClinica.paciente.PacienteRepository;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,8 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("pacientes")
 public class PacienteController {
 
+    @Autowired
+    private PacienteRepository repository;
+
     @PostMapping
-    public void cadastrar(@RequestBody DadosCadastroPaciente dados){
-        System.out.println("dados recebido" + dados);
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados){
+       repository.save((new Paciente(dados)));
     }
 }
