5093e5f7b7a5d79fb760893dac0ac1755077cd67
Vanessazanco <vanessazancof@gmail.com>
2025-01-16 12:06:53 +0000

    Lista de paciente

--- src/main/java/clinica/med/apiClinica/controller/PacienteController.java
+++ src/main/java/clinica/med/apiClinica/controller/PacienteController.java
@@ -7,10 +7,7 @@ import clinica.med.apiClinica.paciente.PacienteRepository;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("pacientes")
@@ -23,5 +20,10 @@ public class PacienteController {
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados){
        repository.save((new Paciente(dados)));
+    }
+
+    @GetMapping
+    public Iterable<Paciente> listar(){
+        return repository.findAll();
     }
 }
