74d24faa627bd40e5a4458dbfc643d52a7e05dbb
Vanessazanco <vanessazancof@gmail.com>
2025-01-16 15:20:06 +0000

    Funcionario lista

--- src/main/java/clinica/med/apiClinica/funcionario/DadosListarFuncionario.java
+++ src/main/java/clinica/med/apiClinica/funcionario/DadosListarFuncionario.java
@@ -1,13 +1,18 @@
 package clinica.med.apiClinica.funcionario;
 
 public record DadosListarFuncionario(
+        Long id,
         String nome,
         String email,
         String crm,
         Especialidade especialidade
 ) {
     public DadosListarFuncionario(Funcionario funcionario) {
-        this(funcionario.getNome(), funcionario.getEmail(), funcionario.getCrm(), funcionario.getEspecialidade());
+        this(   funcionario.getId(),
+                funcionario.getNome(),
+                funcionario.getEmail(),
+                funcionario.getCrm(),
+                funcionario.getEspecialidade());
     }
 
 }
--- src/main/java/clinica/med/apiClinica/paciente/DadosListagemPaciente.java
+++ src/main/java/clinica/med/apiClinica/paciente/DadosListagemPaciente.java
@@ -2,6 +2,8 @@ package clinica.med.apiClinica.paciente;
 
 public record DadosListagemPaciente(String nome, String email, String cpf) {
     public DadosListagemPaciente(Paciente paciente) {
-        this(paciente.getNome(), paciente.getEmail(), paciente.getCpf());
+        this(   paciente.getNome(),
+                paciente.getEmail(),
+                paciente.getCpf());
     }
 }
